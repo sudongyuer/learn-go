b9b94c16aeb9af531b426470305d6c2a80a51558
Frozen FIsh <976499226@qq.com>
2022-08-26 22:20:24 +0800

    chore: update

--- hello/hello.go
+++ hello/hello.go
@@ -1,23 +1,23 @@
-package main
-
-import "fmt"
-
-import "rsc.io/quote"
-
-func main() {
-    fmt.Println(quote.Go())
-}
-
 // package main
 
-// import (
-//     "fmt"
+// import "fmt"
 
-//     "example.com/greetings"
-// )
+// import "rsc.io/quote"
 
 // func main() {
-//     // Get a greeting message and print it.
-//     message := greetings.Hello("Sudongyuer")
-//     fmt.Println(message)
+//     fmt.Println(quote.Go())
 // }
+
+package main
+
+import (
+    "fmt"
+
+    "example.com/greetings"
+)
+
+func main() {
+    // Get a greeting message and print it.
+    message := greetings.Hello("Sudongyuer")
+    fmt.Println(message)
+}
