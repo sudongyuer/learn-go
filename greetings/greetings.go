e24de539686e1cd50ab5b32fea544af2c53616eb
Frozen FIsh <976499226@qq.com>
2022-08-26 23:28:49 +0800

    chore: update

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
     "errors"
     "fmt"
+    "math/rand"
+    "time"
 )
 
 // Hello returns a greeting for the named person.
@@ -13,6 +15,26 @@ func Hello(name string) (string,error) {
     }
 
     // Return a greeting that embeds the name in a message.
-    message := fmt.Sprintf("Hi, %v. Welcome!", name)
+    message := fmt.Sprintf(randomFormat(), name)
     return message, nil
+}
+
+// init sets initial values for variables used in the function.
+func init() {
+    rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string {
+    // A slice of message formats.
+    formats := []string{
+        "Hi, %v. Welcome!",
+        "Great to see you, %v!",
+        "Hail, %v! Well met!",
+    }
+
+    // Return a randomly selected message format by specifying
+    // a random index for the slice of formats.
+    return formats[rand.Intn(len(formats))]
 }
--- hello/hello.go
+++ hello/hello.go
@@ -23,7 +23,7 @@ func main() {
     log.SetPrefix("greetings: ")
     log.SetFlags(0)
 
-    message, err := greetings.Hello("")
+    message, err := greetings.Hello("Gladys")
     // If an error was returned, print it to the console and
     // exit the program.
     if err != nil {
