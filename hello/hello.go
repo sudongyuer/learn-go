73f186ed5d890756e3383a0f00219acd0a11445a
Frozen FIsh <976499226@qq.com>
2022-08-26 22:36:49 +0800

    chore: practice error handle

--- hello/hello.go
+++ hello/hello.go
@@ -12,12 +12,25 @@ package main
 
 import (
     "fmt"
-
+    "log"
     "example.com/greetings"
 )
 
 func main() {
-    // Get a greeting message and print it.
-    message := greetings.Hello("Sudongyuer")
+     // Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+    
+    message, err := greetings.Hello("")
+    // If an error was returned, print it to the console and
+    // exit the program.
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // If no error was returned, print the returned message
+    // to the console.
     fmt.Println(message)
 }
