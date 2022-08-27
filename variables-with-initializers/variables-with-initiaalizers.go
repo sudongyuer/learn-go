1f5f6851cdbd0485575747ceca0eb44ea333adfe
Frozen FIsh <976499226@qq.com>
2022-08-27 20:35:20 +0800

    chore: update

--- variables-with-initializers/variables-with-initiaalizers.go
+++ variables-with-initializers/variables-with-initiaalizers.go
@@ -1,3 +1,14 @@
+// package main
+
+// import "fmt"
+
+// var i, j int = 1, 2
+
+// func main() {
+// 	var c, python, java = true, false, "no!"
+// 	fmt.Println(i, j, c, python, java)
+// }
+
 package main
 
 import "fmt"
