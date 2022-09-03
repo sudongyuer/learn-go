aae43097c671087e8640652abbac2158831b6255
Frozen FIsh <976499226@qq.com>
2022-09-03 21:55:26 +0800

    chore: update

--- slices-33/slice.go
+++ slices-33/slice.go
@@ -2,9 +2,22 @@ package main
 
 import "fmt"
 
-func main() {
+func fn() {
 	primes := [6]int{2, 3, 4, 5, 6, 7}
 	var s []int = primes[0:3]
+	fmt.Println(s, len(s), cap(s))
 
-	fmt.Println(s)
+	s = s[:6]
+	fmt.Println(s, len(s), cap(s))
+}
+
+func main() {
+	var arr = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	var arr1 = [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	var arr2 = arr1
+	arr2[0] = 100
+	fmt.Println(arr, len(arr), cap(arr))
+	fmt.Println(arr1, len(arr1), cap(arr1))
+	fmt.Println(arr2, len(arr2), cap(arr2))
+	fmt.Println(arr1 == arr2)
 }
--- structs-29/structs.go
+++ structs-29/structs.go
@@ -8,5 +8,8 @@ type Vertex struct {
 }
 
 func main() {
+	var v1 = Vertex{1, 2}
+	var v2 = v1
 	fmt.Println(Vertex{1, 2})
+	fmt.Println(v1 == v2)
 }
