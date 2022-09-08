0dc7234560c51c51ccf6a4e38bccadbe59d58fc7
Frozen FIsh <976499226@qq.com>
2022-09-08 23:28:36 +0800

    chore: update

---
+++ range-and-close-56/range-and-close.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func fibonacci(n int, c chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
+	}
+	close(c)
+}
+
+func main() {
+	c := make(chan int)
+	go fibonacci(cap(c), c)
+
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+
+}
